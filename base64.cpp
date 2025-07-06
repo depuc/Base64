91450e681604ac38377ffd98e50dbcb49201fbf2
depuc <koripallipraveendeepak@gmail.com>
2025-07-06 23:39:43 +0530

    Added a new function to read files

--- base64.cpp
+++ base64.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include <map>
+#include <vector>
+#include <fstream>
+std::map<int,char> base64;
 
-
-int main(){
+void init_base64_index_table(){
   
-    std::map<int,char> base64;
    
     int index = 0;
 
@@ -14,10 +15,44 @@ int main(){
     base64[index++] = '+';
     base64[index++] = '/';
     
-    for(std::map<int,char>::iterator it = base64.begin(); it != base64.end(); it++){
-        std::cout << "Key: " << it->first <<" Value: " << it->second<<'\n';
+}
+
+std::vector<char> read_file(const std::string& filename){
+
+    std::ifstream file(filename, std::ios::binary);
+
+    if(!file){
+        std::cerr<< "Error: cannot open file" << filename << '\n';
+        return {};
     }
 
-    return 0;
+    file.seekg(0,std::ios::end);
+    std::streamsize size = file.tellg();
+    file.seekg(0,std::ios::beg);
+
+    std::vector<char> buffer(size);
+
+    if(!file.read(buffer.data(),size)){
+       std::cerr<< "Error: Failed to read file" << filename <<'\n';  
+       return {};
+    }
+
+    return buffer;
+}
+
+
+
+int main(){
+   
+    init_base64_index_table();
+    std::string filename = "README.md";  
+    std::vector<char> binaryData = read_file(filename);
+
+    std::cout << "Read " << binaryData.size() << " bytes.\n";
+
+    for (size_t i = 0; i < std::min(binaryData.size(), size_t(10)); ++i) {
+        printf("%02X ", static_cast<unsigned char>(binaryData[i]));
+    }
+    std::cout << std::endl;
 }
 
